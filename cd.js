f300ecc16fce331bf2684dde56c39507809f7f06
Cheri <avyishi@gmail.com>
2015-07-06 23:59:28 -0700

    Added second countdown timer

--- cd.js
+++ cd.js
@@ -20,13 +20,47 @@ function updateCount(end, da, ho, mi, se) {
     mi.innerHTML = cd[2] + (cd[2] == 1 ? " minute" : " : ");
     se.innerHTML = cd[3] + (cd[3] == 1 ? " second" : "  ");
 
+
 }
 function startCount(end, da, ho, mi, se) {
     updateCount(end, da, ho, mi, se);
     setInterval( function() { updateCount(end, da, ho, mi, se); }, 1000 );
 }
 
+/* JS for Event Name #2 */
 
+CountDownTimer('July 24, 2015', 'countdown');
 
+function CountDownTimer(date, id) {
+    var end       = new Date(date);
+    
+    var second   = 1000;
+    var minute   = second * 60;
+    var hour     = minute * 60;
+    var day      = hour * 24;
+    var timer;
+    
+    var updateCount2 = function() {
+        var now = new Date();
+        var cd = countdown(end);
+        var cd = end - now;
 
+        if (cd < 0) {   
+            clearInterval(countd);
+            document.getElementById(id).innerHTML = 'July 24 is here :(';
+            return;
+        }
 
+        var days = Math.floor(cd / day);
+        var hours = Math.floor((cd % day) / hour);
+        var minutes = Math.floor((cd % hour) / minute);
+        var seconds = Math.floor((cd % minute) / second);
+        
+        document.getElementById(id).innerHTML = days + ' : ';
+        document.getElementById(id).innerHTML += hours + ' : ';
+        document.getElementById(id).innerHTML += minutes + ' : ';
+        document.getElementById(id).innerHTML += seconds + ' ';
+    };
+    
+    countd = setInterval(updateCount2, 1000);
+}
